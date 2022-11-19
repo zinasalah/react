19915e685b9b68cc136a6e7ac87f433516758318
zinasalah <salahzina@live.fr>
2022-11-19 19:22:11 +0100

    first project

---
+++ App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import log from './image/log.jpg';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Form > <
+        img src = { log }
+        alt = "log" / >
+        <
+        Form.Group className = "mb-3"
+        controlId = "formBasicEmail" >
+        <
+        Form.Label > Email address < /Form.Label> <
+        Form.Control type = "email"
+        placeholder = "Enter email" / >
+        <
+        Form.Text className = "text-muted" >
+        We 'll never share your email with anyone else. < /
+        Form.Text > <
+        /Form.Group>
+
+        <
+        Form.Group className = "mb-3"
+        controlId = "formBasicPassword" >
+        <
+        Form.Label > Password < /Form.Label> <
+        Form.Control type = "password"
+        placeholder = "Password" / >
+        <
+        /Form.Group> <
+        Form.Group className = "mb-3"
+        controlId = "formBasicCheckbox" >
+        <
+        Form.Check type = "checkbox"
+        label = "Check me out" / >
+        <
+        /Form.Group> <
+        button controlId = "formBasicSubmit"
+        type = "submit" >
+        Submit <
+        /button> < /
+        Form > < /
+        div >
+    );
+}
+
+export default App;
